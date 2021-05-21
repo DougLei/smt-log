ff6571f0b65cf19e40701aa812d36a1a53e52668
DougLei <953034206@qq.com>
2021-05-21 11:24:00 +0800

    tt

--- src/main/java/com/smt/log/LogController.java
+++ src/main/java/com/smt/log/LogController.java
@@ -2,13 +2,17 @@ package com.smt.log;
 
 import java.util.Date;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.douglei.tools.web.HttpUtil;
 import com.smt.parent.code.filters.log.LogOperation;
+import com.smt.parent.code.filters.token.TokenContext;
 import com.smt.parent.code.response.Response;
 
 /**
@@ -23,26 +27,28 @@ public class LogController {
 	private LogService service;
 	
 	/**
-	 * 记录日志
+	 * (不验证token)记录日志
 	 * @param operation
-	 * @return
-	 */
-	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public Response add(@RequestBody LogOperation operation) {
-		if(operation.getOperDate() == null)
-			operation.setOperDate(new Date());
-		
-		service.add(operation);
-		return new Response(operation);
-	}
-	
-	/**
-	 * 记录日志(不验证token)
-	 * @param operation
+	 * @param request
 	 * @return
 	 */
 	@RequestMapping(value="/add_", method=RequestMethod.POST)
-	public Response addBuiltIn(@RequestBody LogOperation operation) {
-		return add(operation);
+	public Response add_(@RequestBody LogOperation operation, HttpServletRequest request) {
+		return service.add(operation);
+	}
+	
+	/**
+	 * 记录日志
+	 * @param operation
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public Response add(@RequestBody LogOperation operation, HttpServletRequest request) {
+		operation.setUserId(TokenContext.get().getUserId());
+		operation.setOperDate(new Date());
+		operation.setClientIp(HttpUtil.getClientIp(request));
+		
+		return service.add(operation);
 	}
 }
--- src/main/java/com/smt/log/LogService.java
+++ src/main/java/com/smt/log/LogService.java
@@ -7,6 +7,7 @@ import com.douglei.orm.context.Transaction;
 import com.douglei.orm.context.TransactionComponent;
 import com.smt.parent.code.filters.log.LogOperation;
 import com.smt.parent.code.filters.log.LogRequest;
+import com.smt.parent.code.response.Response;
 
 /**
  * 
@@ -21,7 +22,7 @@ public class LogService {
 	 * @return 
 	 */
 	@Transaction
-	public void add(LogOperation operation) {
+	public Response add(LogOperation operation) {
 		Object[] old = operation.getBatch()==null?null:SessionContext.getSqlSession().uniqueQuery_("select id from log_operation where batch=?", Arrays.asList(operation.getBatch()));
 		if(old == null) {
 			SessionContext.getTableSession().save(operation);
@@ -36,5 +37,6 @@ public class LogService {
 				request.setOrder(Integer.parseInt(SessionContext.getSqlSession().uniqueQuery_("select count(id) from log_request where operation_id=?", Arrays.asList(operation.getId()))[0].toString()));
 			SessionContext.getTableSession().save(request);
 		}
+		return new Response(operation.getId());
 	}
 }
